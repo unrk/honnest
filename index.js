e7b84eb49967e6c886f7f28ff678c243058ac6c9
honnest <153329824+unrk@users.noreply.github.com>
2024-03-12 14:17:50 +0100

    tried to implement dark mode button but i'm a skid so idk anything

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 setTimeout(() => {
     document.querySelector('.preloader').style.display = "none"
 }, 2070);
-
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
